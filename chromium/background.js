2779c5c98c9755eca73ca5aad0ff31df518b2d18
Kayo Phoenix <kayo@illumium.org>
2012-10-25 17:27:04 +0600

    Chromium extension refactored..

--- chromium/background.js
+++ chromium/background.js
@@ -1,7 +1,107 @@
-/*chrome.extension.onRequest.addListener(function(request, sender, sendResponse){
-  chrome.pageAction.show(sender.tab.id);
-  sendResponse({});
-});*/
+var defaults = {
+  url: 'http://xhprof.yourdomain/',
+  key: 'secret'
+};
+
+if(!localStorage.opts){
+  localStorage.opts = JSON.stringify(defaults);
+}
+
+var defaults = {
+  state: false,
+  flags: [],
+  source: 'xhprof',
+  types: ['main_frame']
+},
+pool = {},
+filter = {
+  urls: ['<all_urls>']
+};
+
+function args(args){
+  var i, p, ret;
+  if(typeof args == 'string'){
+    ret = {};
+    args = args.split('&');
+    for(i = 0; i < args.length; i++){
+      p = args[i].split('=');
+      ret[p[0]] = decodeURIComponent(p[1]);
+    }
+  }else if(typeof args == 'object'){
+    ret = [];
+    for(i in args){
+      ret.push(i+'='+encodeURIComponent(args[i]));
+    }
+    ret = ret.join('&');
+  }
+  return ret;
+}
+
+function opt(key){
+  return JSON.parse(localStorage.opts)[key];
+}
+
+chrome.webRequest.onBeforeSendHeaders.addListener(function(details){
+  var inst = pool[details.tabId];
+  if(inst && inst.opts.state && inst.opts.types.indexOf(details.type) > -1){
+    details.requestHeaders.push({
+      name: 'X-HProf',
+      value: args({
+        flags: inst.opts.flags.join('+'),
+        source: inst.opts.source,
+        key: opt('key')
+      })
+    });
+    return {requestHeaders: details.requestHeaders};
+  }
+}, filter, ['blocking', 'requestHeaders']);
+
+chrome.webRequest.onHeadersReceived.addListener(function(details){
+  var inst = pool[details.tabId];
+  if(inst && inst.opts.state && inst.opts.types.indexOf(details.type) > -1){
+    for(var i = 0; i < details.responseHeaders.length; i++){
+      if(details.responseHeaders[i].name == 'X-HProf'){
+        var ret = args(details.responseHeaders[i].value);
+        inst.runs.push({
+          href: details.url,
+          type: details.type,
+          run: ret.run,
+          url: opt('url')+'?source='+inst.opts.source+'&run='+ret.run
+        });
+      }
+    }
+  }
+}, filter, ['responseHeaders']);
+
+chrome.extension.onMessage.addListener(function(request, sender, sendResponse){
+  var tabId = request.tabId || sender.tab && sender.tab.id;
+  if(tabId in pool){
+    var inst = pool[tabId];
+    if('opts' in request){
+      if(request.opts){
+        inst.opts = request.opts;
+      }else if(request.opts === true){
+        inst.opts = defaults;
+      }else{
+        sendResponse(inst.opts);
+      }
+    }
+    if('runs' in request){
+      if(request.runs){
+        inst.runs = [];
+      }else{
+        sendResponse(inst.runs);
+      }
+    }
+  }
+});
+
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab){
-  chrome.pageAction.show(tabId);
+  if(changeInfo.status == 'complete'){
+    pool[tabId] = pool[tabId] || {
+      runs: [],
+      opts: defaults
+    };
+    chrome.pageAction.show(tabId);
+  }
 });
--- chromium/popup.js
+++ chromium/popup.js
@@ -1,50 +1,70 @@
-window.addEventListener("load", function(){
-  chrome.tabs.getSelected(null, function(tab) {
-    var flags = document.getElementsByName('flags[]');
-    function flags_changed(){
-      var ck = [];
-      for(var i = 0; i < flags.length; i++){
-        if(flags[i].checked){
-          ck.push(flags[i].value);
-        }
-      }
-      chrome.cookies.set({
-        url: tab.url,
-        name: 'xhprof:flags',
-        path: '/',
-        value: ck.join('+')
-      }, function(){});
+function checklist(boxes, list, i){
+  if(list instanceof Array){
+    for(i = 0; i < boxes.length; i++){
+      boxes[i].checked = list.indexOf(boxes[i].value) > -1;
     }
-    chrome.cookies.get({
-      url: tab.url,
-      name: 'xhprof:flags'
-    }, function(cookie){
-      if(cookie){
-        var ck = cookie.value.split('+');
-        for(var i = 0; i < flags.length; i++){
-          flags[i].checked = ck.indexOf(flags[i].value) > -1;
-        }
+  }else{
+    list = [];
+    for(i = 0; i < boxes.length; i++){
+      if(boxes[i].checked){
+        list.push(boxes[i].value);
       }
-      for(var i = 0; i < flags.length; i++){
-        flags[i].addEventListener('click', flags_changed, false);
+    }
+    return list;
+  }
+}
+
+window.addEventListener('load', function(){
+  chrome.tabs.getSelected(null, function(tab) {
+    var opts_set = document.getElementById('opts'),
+    state = document.getElementsByName('state')[0],
+    flags = document.getElementsByName('flags[]'),
+    source = document.getElementsByName('source')[0],
+    types = document.getElementsByName('types[]'),
+    runs_set = document.getElementById('runs');
+
+    function conf(){
+      chrome.extension.sendMessage({
+        tabId: tab.id,
+        opts:{
+          state: state.checked,
+          flags: checklist(flags),
+          source: source.value,
+          types: checklist(types)
+        }
+      });
+    }
+
+    chrome.extension.sendMessage({
+      tabId: tab.id,
+      opts:null
+    }, function(opts, i){
+      state.checked = opts.state;
+      checklist(flags, opts.flags);
+      source.value = opts.source;
+      checklist(types, opts.types);
+
+      state.addEventListener('click', conf, false);
+      for(i = 0; i < flags.length; i++){
+        flags[i].addEventListener('click', conf, false);
+      }
+      source.addEventListener('keyup', conf, false);
+      for(i = 0; i < types.length; i++){
+        types[i].addEventListener('click', conf, false);
       }
     });
-    var source = document.getElementsByName('source')[0];
-    chrome.cookies.get({
-      url: tab.url,
-      name: 'xhprof:source'
-    }, function(cookie){
-      if(cookie){
-        source.value = cookie.value;
+
+    chrome.extension.sendMessage({
+      tabId: tab.id,
+      runs:null
+    }, function(runs){
+      var links = [];
+      for(var i = 0; i < runs.length; i++){
+        links.push('<a href="'+runs[i].url+'">'+runs[i].run+'</a>');
       }
-      source.addEventListener('keyup', function(){
-        chrome.cookies.set({
-          url: tab.url,
-          name: 'xhprof:source',
-          path: '/',
-          value: source.value
-        }, function(){});
-      }, false);
+      if(links.length){
+        runs_set.innerHTML = '<ul><li>'+links.join('</li><li>')+'</li></ul>';
+      }
     });
   });
 });
